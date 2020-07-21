8445976f0b8b1f8591b0707cded02271e70d2b98
Ronald Ruiz <rrrv1982@gmail.com>
2020-07-21 17:08:25 -0500

    Actualizar.

--- R_Programming_Week_1.R
+++ R_Programming_Week_1.R
@@ -30,3 +30,36 @@ unclass(x)
 x = factor(c("yes", "yes", "no", "yes","no"),
            levels = c("yes","no"))
 x
+
+##VALORES PERDIDOS (Missing values)
+x = c(1, 2, NA, 10, 3)
+is.na(x)
+is.nan(x)
+x = c(1, 2, NA, NaN, 3)
+is.na(x)
+is.nan(x)
+
+
+##DATAFRAMES
+#Se pueden llamar con la funcion read.table o read.csv
+x = data.frame(foo = 1:4, bar = c(T,T,T,F))
+x
+nrow(x)
+ncol(x)
+
+##ATRIBUTO DE NOMBRES (Names attribute)
+#Nombres en un vector
+y = 1:3
+names(y)
+names(y) = c("foo", "bar", "norf")
+y
+names(y)
+
+#nombres en una lista
+y = list(a= 1, b=2, c=3)
+y
+
+#nombres en una matriz
+m = matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m) = list(c("a", "b"), c("c", "d"))
+m
